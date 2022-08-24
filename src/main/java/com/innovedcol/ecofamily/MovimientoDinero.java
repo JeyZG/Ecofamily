895353bef05a2bf31075bf170e5ec3e746892a9f
Alex <alexander.carvajalo@udea.edu.co>
2022-08-23 22:40:07 -0500

    Cree la clase movimientoDinero

---
+++ src/main/java/com/innovedcol/ecofamily/MovimientoDinero.java
@@ -0,0 +1,49 @@
+package com.innovedcol.ecofamily;
+
+public class MovimientoDinero {
+
+    private double monto;
+    private String concepto;
+    private String  tipoMovimiento;
+    private String usuario;
+
+    public MovimientoDinero(double monto, String concepto, String tipoMovimiento, String usuario) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.tipoMovimiento = tipoMovimiento;
+        this.usuario = usuario;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+
+    public void setTipoMovimiento(String tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+}
