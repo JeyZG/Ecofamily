39d32aa4250dddf76dd192bc39108b10b7346ff8
Carlos <Carlx3015@gmail.com>
2022-08-23 21:40:11 -0500

    Verificación y prueba de la clase empleado

--- src/main/java/com/innovedcol/ecofamily/Main.java
+++ src/main/java/com/innovedcol/ecofamily/Main.java
@@ -2,7 +2,19 @@ package com.innovedcol.ecofamily;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hola mundo!");
+
+        //TEST//
+
+        //Clase empleado:
+        Empleado emp= new Empleado("Mario","XXXX@gmail.com","EcoFamily","Almacenista");
+        System.out.println(emp.toString());
+
+        emp.setNombre("Hellder");
+        emp.setCorreo("cccc@gmail.com");
+        emp.setRol("Contador");
+        emp.setEmpresaContratante("EcoFamily2");
+        System.out.println(emp.toString()); //Cambios de datos.
+
     }
 }
